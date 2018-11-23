fc29a9b67d9f2100f81023518d87398ebcdf9b07
DevExpressExampleBot <fakeEmail>
2018-11-23 15:06:19 +0300

    Source auto update [skip ci]

---
+++ VB/Create a Report Bound to XPObjectSource/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿Namespace Create_a_Report_Bound_to_XPObjectSource
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton3 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton4 = New DevExpress.XtraEditors.SimpleButton()
+            Me.SuspendLayout()
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Location = New System.Drawing.Point(46, 34)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(186, 23)
+            Me.simpleButton1.TabIndex = 0
+            Me.simpleButton1.Text = "Show Print Preview"
+            ' 
+            ' simpleButton2
+            ' 
+            Me.simpleButton2.Location = New System.Drawing.Point(46, 88)
+            Me.simpleButton2.Name = "simpleButton2"
+            Me.simpleButton2.Size = New System.Drawing.Size(186, 23)
+            Me.simpleButton2.TabIndex = 1
+            Me.simpleButton2.Text = "Run Report Designer"
+            ' 
+            ' simpleButton3
+            ' 
+            Me.simpleButton3.Location = New System.Drawing.Point(46, 143)
+            Me.simpleButton3.Name = "simpleButton3"
+            Me.simpleButton3.Size = New System.Drawing.Size(186, 23)
+            Me.simpleButton3.TabIndex = 2
+            Me.simpleButton3.Text = "Create XPObjectSource Dynamically"
+            ' 
+            ' simpleButton4
+            ' 
+            Me.simpleButton4.Location = New System.Drawing.Point(46, 198)
+            Me.simpleButton4.Name = "simpleButton4"
+            Me.simpleButton4.Size = New System.Drawing.Size(186, 23)
+            Me.simpleButton4.TabIndex = 3
+            Me.simpleButton4.Text = "Resolve/Dismiss Session"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(284, 261)
+            Me.Controls.Add(Me.simpleButton4)
+            Me.Controls.Add(Me.simpleButton3)
+            Me.Controls.Add(Me.simpleButton2)
+            Me.Controls.Add(Me.simpleButton1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton3 As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton4 As DevExpress.XtraEditors.SimpleButton
+    End Class
+End Namespace
+
