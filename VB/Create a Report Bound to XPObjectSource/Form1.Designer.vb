b7affc8a1ecfba14126853bc63d6648556e69c43
DevExpressExampleBot <fakeEmail>
2021-10-28 16:35:27 +0300

    autogenerate VB

--- VB/Create a Report Bound to XPObjectSource/Form1.Designer.vb
+++ VB/Create a Report Bound to XPObjectSource/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace Create_a_Report_Bound_to_XPObjectSource
-    Partial Public Class Form1
+Namespace Create_a_Report_Bound_to_XPObjectSource
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -36,34 +38,62 @@
             Me.simpleButton1.Size = New System.Drawing.Size(186, 23)
             Me.simpleButton1.TabIndex = 0
             Me.simpleButton1.Text = "Show Print Preview"
-            ' 
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click)
+'''  ' 
             ' simpleButton2
             ' 
-            Me.simpleButton2.Location = New System.Drawing.Point(46, 88)
+Me.simpleButton2.Location = New System.Drawing.Point(46, 88)
             Me.simpleButton2.Name = "simpleButton2"
             Me.simpleButton2.Size = New System.Drawing.Size(186, 23)
             Me.simpleButton2.TabIndex = 1
             Me.simpleButton2.Text = "Run Report Designer"
-            ' 
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click)
+'''  ' 
             ' simpleButton3
             ' 
-            Me.simpleButton3.Location = New System.Drawing.Point(46, 143)
+Me.simpleButton3.Location = New System.Drawing.Point(46, 143)
             Me.simpleButton3.Name = "simpleButton3"
             Me.simpleButton3.Size = New System.Drawing.Size(186, 23)
             Me.simpleButton3.TabIndex = 2
             Me.simpleButton3.Text = "Create XPObjectSource Dynamically"
-            ' 
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click)
+'''  ' 
             ' simpleButton4
             ' 
-            Me.simpleButton4.Location = New System.Drawing.Point(46, 198)
+Me.simpleButton4.Location = New System.Drawing.Point(46, 198)
             Me.simpleButton4.Name = "simpleButton4"
             Me.simpleButton4.Size = New System.Drawing.Size(186, 23)
             Me.simpleButton4.TabIndex = 3
             Me.simpleButton4.Text = "Resolve/Dismiss Session"
-            ' 
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click)
+'''  ' 
             ' Form1
             ' 
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(284, 261)
             Me.Controls.Add(Me.simpleButton4)
@@ -72,16 +102,17 @@
             Me.Controls.Add(Me.simpleButton1)
             Me.Name = "Form1"
             Me.Text = "Form1"
+            AddHandler Me.Click, New System.EventHandler(AddressOf Me.simpleButton1_Click)
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+#End Region
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
 
-        Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
-        Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
-        Private WithEvents simpleButton3 As DevExpress.XtraEditors.SimpleButton
-        Private WithEvents simpleButton4 As DevExpress.XtraEditors.SimpleButton
+        Private simpleButton2 As DevExpress.XtraEditors.SimpleButton
+
+        Private simpleButton3 As DevExpress.XtraEditors.SimpleButton
+
+        Private simpleButton4 As DevExpress.XtraEditors.SimpleButton
     End Class
 End Namespace
-
